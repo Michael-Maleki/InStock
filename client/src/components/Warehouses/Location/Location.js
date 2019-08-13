7e00eb1b23330954ce78ddc78c2b862e73822a96
Dale Shlass <dale@shlass.com>
2019-08-13 17:38:06 -0400

    tablet styles completed. working on Desktop now. axios request to be completed once backend is set up.

--- client/src/components/Warehouses/Location/Location.js
+++ client/src/components/Warehouses/Location/Location.js
@@ -10,13 +10,23 @@ class Location extends React.Component {
     return(
       <li className="location__item" id={id}>
         <div className='location__content-flex'>
-          <p className="location__text location__text--bold">{name}</p>
-          <p className="location__text location__text--padding">{address.street}</p>
-          <p className="location__text">{contact.name}</p>
-          <p className="location__text location__text--padding">{contact.title}</p>
-          <p className="location__text">{contact.phone}</p>
-          <a className="location__mail location__text--padding" href="mailto:info@thebeesknees.com">{contact.email}</a>
-          <p className="location__text">{inventoryCategories}</p>
+          <div className='location__top-flex'>
+            <p className="location__text location__text--bold">{name}</p>
+            <p className="location__text location__text--padding">{address.street}</p>
+          </div>
+          <div className='location__bottom-flex'>
+            <span className='location__text-container--name'>
+              <p className="location__text">{contact.name}</p>
+              <p className="location__text location__text--padding">{contact.title}</p>
+            </span>
+            <span className='location__text-container'>
+              <p className="location__text">{contact.phone}</p>
+              <a className="location__mail location__text--padding" href="mailto:info@thebeesknees.com">{contact.email}</a>
+            </span>
+            <span className='location__text-container'>
+              <p className="location__text">{inventoryCategories}</p>
+            </span>
+          </div>
         </div>
         {/* <Link to='/'> */}
           <img className='location__arrow'src={rightArrow} alt='right-arrow'/>
