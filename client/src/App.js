3780793b1208c7d05aed4a982f3a8b8b23569db9
Dale Shlass <dale@shlass.com>
2019-08-14 18:12:00 -0400

    Warehouse view component added and onclick functionality added to bring state to warehouse view.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Warehouses from './components/Warehouses/Warehouses.js'
+import WarehouseView from './components/WarehouseView/WarehouseView.js'
 import Inventory from './components/Inventory/Inventory.js'
 
 class App extends React.Component {
@@ -9,15 +10,20 @@ class App extends React.Component {
     return `http://localhost:8080${endpoint}`
   }
 
+  handleWarehouseClick= (data) => {
+    console.log(data)
+      this.setState({locationData: data})
+  }
+
   render() {
     return (
       <Router>
         <Switch>
-          <Route path='/warehouse/:warehouseId' render={(props) => <Warehouses match={props.match} urlBuilder={this.urlBuilder}/>}/>
+          <Route path='/warehouse/:warehouseId' render={(props) => <WarehouseView match={props.match} urlBuilder={this.urlBuilder} locationData={this.state.locationData}/>}/>
           <Route path='/warehouse' render={(props) => <Warehouses match={props.match} urlBuilder={this.urlBuilder}/>}/>
           <Route path='/inventory/:inventoryId' render={(props) => <Warehouses match={props.match} urlBuilder={this.urlBuilder}/>}/>
           <Route path='/inventory' render={(props) => <Inventory match={props.match} urlBuilder={this.urlBuilder}/>}/>
-          <Route path='/' render={(props) => <Warehouses match={props.match} urlBuilder={this.urlBuilder}/>}/>
+          <Route path='/' render={(props) => <Warehouses match={props.match} urlBuilder={this.urlBuilder} handleWarehouseClick={this.handleWarehouseClick}/>}/>
         </Switch>
       </Router>
     );
--- client/src/components/WarehouseView/WarehouseView.js
+++ client/src/components/WarehouseView/WarehouseView.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import axios from 'axios';
 // import {Link} from 'react-router-dom';
-import Location from './Location/Location.js'
+import InventoryList from '../Inventory/InventoryList'
 // import locations from '../../data/locations.json'
 import Header from '../Header/Header.js'
 
-class Warehouses extends React.Component {
+class WarehouseView extends React.Component {
   
     state= {
       isLoaded: false
     }
 
-    getWarehouses = () => {
-      axios.get(this.props.urlBuilder('/warehouse'))
+    getInventoryList = () => {
+      axios.get(this.props.urlBuilder(`/warehouse/${this.props.match.params.warehouseId}`))
       .then(resp => {
           const {data} = resp
           this.setState({
               isLoaded:true,
-              locationData: data
+              locationData: this.props.locationData,
+              warehouseInventory: data
           })
       }).catch(error => {
         this.setState({
@@ -28,11 +29,11 @@ class Warehouses extends React.Component {
     }
 
     componentDidMount() {
-      this.getWarehouses()
+      this.getInventoryList()
     }
   
   render() {
-
+    console.log(this.state.locationData)
     const {isLoaded, error} = this.state
 
     if (error) {
@@ -58,28 +59,24 @@ class Warehouses extends React.Component {
     return(
       <>
       <Header />
-      <main className='warehouse'>
-        <header className='warehouse__header'>
-          <h1 className='warehouse__title'>Locations</h1>
+      <section className="inventory">
+        <div className= "inventory__top">
+          <h1 className="inventory--heading">Inventory</h1>
           <input className='warehouse__search' type='search' placeholder='Search'/>
-        </header>
-        <ul className='warehouse__label-container'>
-          <li className='warehouse__label warehouse__label--name'>Warehouse</li>
-          <li className='warehouse__label warehouse__label--contact'>Contact</li>
-          <li className='warehouse__label warehouse__label--contact-info'>Contact Information</li>
-          <li className='warehouse__label warehouse__label--categiories'>Categories</li>
-        </ul>
-        <ul className='warehouse__list'>
-          {
-          
-          this.state.locationData.map(location => <Location key={location.id} location={location} handleWarehouseClick={this.props.handleWarehouseClick}/>)
-          
-          }
-        </ul>
-      </main>
+        </div>
+        
+        <div className="inventory--categories top-display--categories">
+            <div id="only-item">item</div>
+            <div>last ordered</div>
+            <div>location</div>
+            <div>quantity</div>
+            <div>status</div>
+        </div>
+        <InventoryList listData={this.state.warehouseInventory}/> 
+      </section>
       </>
     )
   }
 }
 
-export default Warehouses
+export default WarehouseView
