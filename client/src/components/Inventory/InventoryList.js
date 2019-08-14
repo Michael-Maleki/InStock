bc3dd5de7a814cc6227e9a027a7d551d7ff1e7a4
noortaz <noortazc@gmail.com>
2019-08-14 14:40:08 -0400

    kebab-button positioned

--- client/src/components/Inventory/InventoryList.js
+++ client/src/components/Inventory/InventoryList.js
@@ -1,36 +1,65 @@
 import React from 'react';
+import removeIcon from '../../assets/Icons/SVG/Icon-kebab-default.svg'
 
 
 class InventoryList extends React.Component {
 
-  
+  state = {
+    showOption: false,
+    className: 'remove-option-hidden'
+  }
+
+  showRemoveOption = () => {
+
+    if (this.state.showOption === true) {
+      this.setState({
+        showOption: !this.state.showOption,
+        className: 'remove-option-hidden'
+      })
+    } else {
+      this.setState({
+        showOption: !this.state.showOption,
+        className: 'remove-option-displayed'
+      })
+    }
+  }
+
 
   render() {
     console.log(this.props.listData[0].name);
+    console.log(this.state.showOption);
+    console.log(this.state.className);
     return (
       <>
         {this.props.listData.map((item) => {
           return(
             <div key={item.id} id={item.id} className="inventory__items">
+              <div className="inventory__content">
+                <div className="inventory__first-item">
+                  <div className="inventory--categories">item</div>
+                  <div className="inventory__products--details product-name">{item.name}</div>
+                  <div className="inventory__products--details product-description">{item.description}</div>
+                </div>
 
-              <div className="inventory__first-item">
-                <div className="inventory--categories">item</div>
-                <div className="inventory__products--details product-name">{item.name}</div>
-                <div className="inventory__products--details product-description">{item.description}</div>
+                <div className="inventory--categories">last ordered</div>
+                <div className="inventory__products--details">{item.lastOrdered}</div>
+
+                <div className="inventory--categories">location</div>
+                <div className="inventory__products--details">{item.location}</div>
+
+                <div className="inventory--categories">quantity</div>
+                <div className="inventory__products--details">{item.quantity}</div>
+
+                <div className="inventory--categories">status</div>
+                <div className="inventory__products--details">{(item.isInstock) ? 'In stock' : 'Out of stock'}</div>
               </div>
 
-              <div className="inventory--categories">last ordered</div>
-              <div className="inventory__products--details">{item.lastOrdered}</div>
-
-              <div className="inventory--categories">location</div>
-              <div className="inventory__products--details">{item.location}</div>
-
-              <div className="inventory--categories">quantity</div>
-              <div className="inventory__products--details">{item.quantity}</div>
-
-              <div className="inventory--categories">status</div>
-              <div className="inventory__products--details">{(item.isInstock) ? 'In stock' : 'Out of stock'}</div>
+              <div className="remove">
+                  <img className="remove-btn-image" onClick={this.showRemoveOption} src={removeIcon} alt=""/>
+                  <button className={this.state.className}>Remove</button>
+              </div>
             </div>
+            
           )
         })}
       </>
