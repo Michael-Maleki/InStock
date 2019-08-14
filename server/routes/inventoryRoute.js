674f21f91e947b6dbcdef61fac4102579d6d2920
Dale Shlass <dale@shlass.com>
2019-08-14 14:57:34 -0400

    inventory item router completed.

--- server/routes/inventoryRoute.js
+++ server/routes/inventoryRoute.js
@@ -4,10 +4,17 @@ const uuid = require('uuid/v1');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const inventory = require('../data/inventory.json')
-const locations = require('../data/locations.json')
 
 router.get('/', (req, res) => {
-    res.send(200).json(inventory);
+    res.status(200).json(inventory);
 });
+
+router.get('/:inventoryId', (req,res) => {
+  
+	let inventoryItem = inventory.filter(inventory => { return inventory.id === req.params.inventoryId})
+  return (!inventoryItem.toString()) ? res.status(404).json({'message': 'Item does not exist'}) : res.status(200).json(inventoryItem)
+
+})
+
 
 module.exports = router;
