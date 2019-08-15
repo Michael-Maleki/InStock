7d821ce9fe72d38f5655293506c0004f2fcf14e7
Assana <bmisrah@gmail.com>
2019-08-15 15:44:34 -0400

    added part of post for backend

--- server/routes/inventoryRoute.js
+++ server/routes/inventoryRoute.js
@@ -1,20 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const uuid = require('uuid/v1');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const inventory = require('../data/inventory.json')
+const uuid = require("uuid/v1");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const inventory = require("../data/inventory.json");
 
-router.get('/', (req, res) => {
-    res.status(200).json(inventory);
+router.get("/", (req, res) => {
+  res.status(200).json(inventory);
 });
 
-router.get('/:inventoryId', (req,res) => {
+router.get("/:inventoryId", (req, res) => {
+  let inventoryItem = inventory.filter(inventory => {
+    return inventory.id === req.params.inventoryId;
+  });
+  return !inventoryItem.toString()
+    ? res.status(404).json({ message: "Item does not exist" })
+    : res.status(200).json(inventoryItem);
+});
 
-	let inventoryItem = inventory.filter(inventory => { return inventory.id === req.params.inventoryId})
-  return (!inventoryItem.toString()) ? res.status(404).json({'message': 'Item does not exist'}) : res.status(200).json(inventoryItem)
-
-})
+router.post("/", (req, res) => {
+  const { id,
+    name,
+    description,
+    quantity,
+    lastOrdered,
+    location,
+    isInstock,
+    categories,
+    warehouseId } 
+}
 
 
 module.exports = router;
