1ca96d31d0072077dc2fa16986bdd6b1e8dcea2a
noortaz <noortazc@gmail.com>
2019-08-18 15:04:56 -0400

    got rid of the incomplete post request

--- server/routes/inventoryRoute.js
+++ server/routes/inventoryRoute.js
@@ -9,7 +9,6 @@ router.get("/", (req, res) => {
   res.status(200).json(inventory);
 });
 
-<<<<<<< HEAD
 router.get("/:inventoryId", (req, res) => {
   let inventoryItem = inventory.filter(inventory => {
     return inventory.id === req.params.inventoryId;
@@ -18,29 +17,9 @@ router.get("/:inventoryId", (req, res) => {
     ? res.status(404).json({ message: "Item does not exist" })
     : res.status(200).json(inventoryItem);
 });
-=======
-router.get('/:inventoryId', (req,res) => {
-  
-	let inventoryItem = inventory.filter(inventory => { return inventory.id === req.params.inventoryId})
-  return (!inventoryItem.toString()) ? res.status(404).json({'message': 'Item does not exist'}) : res.status(200).json(inventoryItem)
->>>>>>> 7ebf6290c21feba6bd4b5ab1052ab489eff7f45b
-
-router.post("/", (req, res) => {
-  const { id,
-    name,
-    description,
-    quantity,
-    lastOrdered,
-    location,
-    isInstock,
-    categories,
-    warehouseId } 
-}
 
 
-<<<<<<< HEAD
-module.exports = router;
-=======
+
 router.delete('/', (req, res) => {
   
   let deletedItemId = req.body.id;
@@ -62,4 +41,3 @@ router.delete('/', (req, res) => {
 
 
 module.exports = router;
->>>>>>> 7ebf6290c21feba6bd4b5ab1052ab489eff7f45b
