014ab2fa7d5457d5c1bafab7c6e228e07e37d5b8
Assana <bmisrah@gmail.com>
2019-08-19 15:12:21 -0400

    added reload function

--- client/src/components/Inventory/Inventory.js
+++ client/src/components/Inventory/Inventory.js
@@ -18,8 +18,7 @@ class Inventory extends React.Component {
       }
     })
     .then((response) => {
-      //console.log(response.data);
-      //window.location.reload();
+      window.location.reload();\
     })
   }
 
